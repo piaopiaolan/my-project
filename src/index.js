9c9d370b3a0637804e5c8540fe9af08e792495fb
coding111 <765411572@qq.com>
2026-01-13 10:16:40 +0800

    初始化项目结构

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// 主入口文件
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = { greet, add };
---
+++ tests/test.js
@@ -0,0 +1,5 @@
+const { greet, add } = require('../src/index.js');
+
+console.log('测试 greet 函数:', greet('World'));
+console.log('测试 add 函数:', add(2, 3));
+console.log('所有测试通过！');
